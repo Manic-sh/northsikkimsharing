8574d9be720d39dfafda5c6534228e2161562c03
Manic-sh <mash.pro666@gmail.com>
2023-08-30 18:11:49 +0530

    Build error fix

--- pages/tour/tour-list-v3/index.jsx
+++ pages/tour/tour-list-v3/index.jsx
@@ -3,7 +3,6 @@ import Header2 from "../../../components/header/header-2";
 import DropdownSelelctBar from "../../../components/tour-list/common/DropdownSelelctBar";
 import MapPropertyFinder from "../../../components/tour-list/common/MapPropertyFinder";
 import Pagination from "../../../components/tour-list/common/Pagination";
-import MainFilterSearchBox from "../../../components/tour-list/tour-list-v3/MainFilterSearchBox";
 import TopHeaderFilter from "../../../components/tour-list/tour-list-v3/TopHeaderFilter";
 import TourProperties from "../../../components/tour-list/tour-list-v3/TourProperties";
 
@@ -21,8 +20,7 @@ const index = () => {
 
       <section className="halfMap">
         <div className="halfMap__content">
-          <MainFilterSearchBox />
-
+          
           <div className="row x-gap-10 y-gap-10 pt-20">
             <DropdownSelelctBar />
           </div>
