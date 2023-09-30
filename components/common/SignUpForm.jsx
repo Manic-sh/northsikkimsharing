100993ac6dd6b676f613c6736f97138c167d8510
rohit133 <rohit.12012980@lpu.in>
2023-09-30 21:13:58 +0530

    Signup functions

--- components/common/SignUpForm.jsx
+++ components/common/SignUpForm.jsx
@@ -1,8 +1,61 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { React, useState } from "react"
 
 const SignUpForm = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    phone_no: "",
+    role: "customer",
+    status: "active",
+  });
+
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("Register");
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      // Passwords do not match, handle the error here
+      setPasswordMatch(false);
+      console.error("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch("https://nss-backend-services.onrender.com/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Handle successful registration, e.g., redirect to another page or show a success message
+        console.log("User registered successfully!");
+        router.push("/"); // Redirect to the home screen path
+      } else {
+        // Handle errors, e.g., show an error message to the user
+        console.error("Registration failed");
+      }
+    } catch (error) {
+      console.error("Error occurred while registering user", error);
+    }
+  };
+
   return (
-    <form className="row y-gap-20">
+    <form className="row y-gap-20" onSubmit={handleSubmit}>
       <div className="col-12">
         <h1 className="text-22 fw-500">Welcome back</h1>
         <p className="mt-10">
@@ -16,7 +69,7 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+          <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} required />
           <label className="lh-1 text-14 text-light-1">First Name</label>
         </div>
       </div>
@@ -24,7 +77,7 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+          <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} required />
           <label className="lh-1 text-14 text-light-1">Last Name</label>
         </div>
       </div>
@@ -32,7 +85,7 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+          <input type="text" name="email" value={formData.email} onChange={handleInputChange} required />
           <label className="lh-1 text-14 text-light-1">Email</label>
         </div>
       </div>
@@ -40,7 +93,15 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="password" required />
+          <input type="text" name="phone_no" value={formData.phone_no} onChange={handleInputChange} required />
+          <label className="lh-1 text-14 text-light-1">Phone</label>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-12">
+        <div className="form-input ">
+          <input type="password" name="password" value={formData.password} onChange={handleInputChange} required />
           <label className="lh-1 text-14 text-light-1">Password</label>
         </div>
       </div>
@@ -48,9 +109,12 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="password" required />
+          <input className={passwordMatch ? "" : "border-danger"} type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange} required />
           <label className="lh-1 text-14 text-light-1">Confirm Password</label>
         </div>
+        {!passwordMatch && (
+          <p className={passwordMatch ? "" : "text-danger"}>Passwords do not match. Please try again.</p>
+        )}
       </div>
       {/* End .col */}
 
@@ -73,7 +137,6 @@ const SignUpForm = () => {
       <div className="col-12">
         <button
           type="submit"
-          href="#"
           className="button py-20 -dark-1 bg-blue-1 text-white w-100"
         >
           Sign Up <div className="icon-arrow-top-right ml-15" />
--- pages/others-pages/signup.js
+++ pages/others-pages/signup.js
@@ -1,8 +1,8 @@
 import dynamic from "next/dynamic";
 import CallToActions from "../../components/common/CallToActions";
 import Seo from "../../components/common/Seo";
-import DefaultHeader from "../../components/header/default-header";
-import DefaultFooter from "../../components/footer/default";
+import Header2 from "../../components/header/header-2";
+import Footer2 from "../../components/footer/footer-2";
 import LoginWithSocial from "../../components/common/LoginWithSocial";
 import SignUpForm from "../../components/common/SignUpForm";
 
@@ -15,7 +15,7 @@ const SignUp = () => {
       <div className="header-margin"></div>
       {/* header top margin */}
 
-      <DefaultHeader />
+      <Header2 />
       {/* End Header 1 */}
 
       <section className="layout-pt-lg layout-pb-lg bg-blue-2">
@@ -49,7 +49,7 @@ const SignUp = () => {
       <CallToActions />
       {/* End Call To Actions Section */}
 
-      <DefaultFooter />
+      <Footer2 />
       {/* End Call To Actions Section */}
     </>
   );
